ca671ce7689623b1f92e3f9b1df0361ed58fe7bc
Aaditya Kumar Mittal <adityakumarashish1909@gmail.com>
2024-04-13 22:17:01 +0530

    Add Procfile for Heroku deployment

---
+++ server/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Navigate to the server directory
+cd server
+
+# Install dependencies
+echo "Installing server dependencies..."
+npm install
+
+# Start the server
+echo "Starting the server..."
+npm start
